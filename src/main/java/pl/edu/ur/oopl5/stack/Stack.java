0329133bec91cf79c504ae41915d530323551ac8
Dawid <kosiordawid@gmail.com>
2019-04-02 11:58:44 +0200

    init

---
+++ src/main/java/pl/edu/ur/oopl5/stack/Stack.java
@@ -0,0 +1,24 @@
+package pl.edu.ur.oopl5.stack;
+
+import java.util.EmptyStackException;   
+
+/**
+ */
+public class Stack extends AbstractStack {
+
+    @Override
+    public void push(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
